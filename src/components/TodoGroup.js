ac88410cc04bc3292a2f3181bc8cbb029feb5385
AubBantolo <jhuliet14@gmail.com>
2023-08-28 20:32:18 +0800

    feat: added map for itemlist in TodoGroup

--- src/components/TodoGroup.js
+++ src/components/TodoGroup.js
@@ -1,10 +1,13 @@
 import { useState } from "react";
 import TodoItem from './TodoItem';
 
-const TodoGroup = () => {
+const TodoGroup = (props) => {
     return (
         <div style={{display:"flex",justifyContent:"center",alignItems:"center"}}>
-       
+           {
+                props.itemList.map((item, index) => 
+                <TodoItem key={index} item={item}></TodoItem>
+           )}
         </div>
     );
 }

e8471b7e3c361cad78e541d63e12a36bd6824be7
AubBantolo <jhuliet14@gmail.com>
2023-08-29 16:20:50 +0800

    feat: added function is item is ticked

--- src/components/TodoItem.js
+++ src/components/TodoItem.js
@@ -1,10 +1,36 @@
 import '../App.css';
+import React, { useState } from "react";
+
 
 const TodoItem = (props) => {
+
+    const [isChecked, setIsChecked] = useState(true);
+
+    const checkKey = () => {
+        
+        const result = {
+            id: props.itemKey,
+            text: props.item,
+            done: isChecked,
+        };
+
+        if(isChecked){
+            console.log(JSON.stringify(result));
+        }
+        else {
+            console.log(props.itemKey + " " + props.item + " Done: " + isChecked);
+        }
+    }
+
+    const handleCheckboxChange = () => {
+        setIsChecked(!isChecked);
+        checkKey();
+    };
+
     return (
         <div className="todoItem">
 
-        <span>{props.item}</span>
+        <span> <input type="checkbox" onChange={handleCheckboxChange}/> {props.item} </span>
          
         </div>
     );

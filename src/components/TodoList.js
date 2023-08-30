d21a553d219a9f304883a31e55157be35d7107c5
Elaine De los Reyes <elaine.leah.marie.de.los.reyes@oocl.com>
2023-08-30 15:22:11 +0800

    feat: implemented axios create

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -3,9 +3,14 @@ import '../App.css';
 import TodoGenerator from './TodoGenerator';
 import TodoGroup from './TodoGroup';
 import { addTodoItem, removeTodoItem } from './todoListSlice';
+import axios from 'axios';
 
 const TodoList = (props) => {
 
+    const api = axios.create({
+        baseURL: 'https://64ee17e31f87218271424d11.mockapi.io/api/v1/'
+    });
+    api.get('/todos').then(response => console.log(response));
     const todoList = useSelector(state => state.todoList.todoList);
     const dispatch = useDispatch();
 
